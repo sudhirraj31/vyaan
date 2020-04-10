59a9ffafc05a75754fa59c02e9339e5755a23952
sudhir raj <sudhirraj31@gmail.com>
2020-04-10 13:29:15 +0530

    webpack config file addition

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var CompressionPlugin = require('compression-webpack-plugin');
+
+plugins: [
+    new webpack.DefinePlugin({ 
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+
+  app.get('*.js', function (req, res, next) {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    next();
+  });
